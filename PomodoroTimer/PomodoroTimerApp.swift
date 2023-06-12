8147898c651cf3df42be25d0fb76b59acba5cac1
James Lea <james.q.lea@gmail.com>
2023-06-12 11:49:20 -0600

    first commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by James Lea on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
